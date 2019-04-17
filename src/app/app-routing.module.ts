0094b75eb3ee7b009da1842b282343b2faabb18f
Morgan Reynolds <morgan.reynolds@wgu.edu>
2019-04-17 11:51:39 -0600

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HeroesComponent} from "./heroes/heroes.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'heroes', // -> localhost:4200/heroes
+    component: HeroesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
